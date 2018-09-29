6f4eb1eb3c7ed9dd5fb27eb4eb532bae83d335bd
jomof <jomofisher@gmail.com>
2018-09-29 14:09:14 -0700

    Ninja

--- app/src/main/java/com/example/androidccacheexample/MainActivity.java
+++ app/src/main/java/com/example/androidccacheexample/MainActivity.java
@@ -6,14 +6,19 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    // Used to load the 'native-lib' library on application startup.
+    static {
+        System.loadLibrary("native-lib");
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-    // Example of a call to a native method
-    TextView tv = findViewById(R.id.sample_text);
-    tv.setText(stringFromJNI());
+        // Example of a call to a native method
+        TextView tv = findViewById(R.id.sample_text);
+        tv.setText(stringFromJNI());
     }
 
     /**
@@ -21,9 +26,4 @@ public class MainActivity extends AppCompatActivity {
      * which is packaged with this application.
      */
     public native String stringFromJNI();
-
-    // Used to load the 'native-lib' library on application startup.
-    static {
-        System.loadLibrary("native-lib");
-    }
 }
